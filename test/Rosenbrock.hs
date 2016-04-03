e56d43e42e56fd580cb98c932957e2cd90dec51c
Jared Tobin <jared@jtobin.ca>
2016-04-03 10:50:10 +0700

    src -> lib, etc.

--- test/Rosenbrock.hs
+++ test/Rosenbrock.hs
@@ -11,11 +11,13 @@ rosenbrock xs = negate (5  *(x1 - x0 ^ 2) ^ 2 + 0.05 * (1 - x0) ^ 2) where
   [x0, x1] = V.toList xs
 
 main :: IO ()
-main = withSystemRandom . asGenIO $ mcmc 100 ensemble rosenbrock where
+main = withSystemRandom . asGenIO $ mcmc 10 ensemble rosenbrock where
   ensemble = V.fromList [
       V.fromList [negate 1.0 :: Double, negate 1.0]
     , V.fromList [negate 1.0, 1.0]
     , V.fromList [1.0, negate 1.0]
     , V.fromList [1.0, 1.0]
     ]
+
+-- ensemble = V.fromList [ V.fromList [negate 1.0 :: Double, negate 1.0], V.fromList [negate 1.0, 1.0], V.fromList [1.0, negate 1.0], V.fromList [1.0, 1.0]]
 
